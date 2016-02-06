fa84e8b6762aee78b317bcbe0968cab4d4e18c45
Luis Font <fontluis@gmail.com>
2016-02-05 18:17:42 -0600

    Added routing and partial views

---
+++ InmobiliariaGoni/app/Controllers/houseDetailCtrl.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("realEstateManagement").controller("HouseDetailCtrl", ["house", HouseDetailCtrl]);
+
+    function HouseDetailCtrl(house) {
+        var self = this;
+
+        self.house = house;
+
+        if (self.house.tags) {
+            self.house.tagList = self.house.tags.toString();
+        }
+    }
+}());
